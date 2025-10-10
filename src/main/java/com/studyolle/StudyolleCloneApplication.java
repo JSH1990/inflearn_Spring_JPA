9ab50696b2d171f03a06d919a868928c7539e0bf
jshblack <118979741+JSH1990@users.noreply.github.com>
2025-10-10 23:09:23 +0900

    first

---
+++ src/main/java/com/studyolle/StudyolleCloneApplication.java
@@ -0,0 +1,13 @@
+package com.studyolle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyolleCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyolleCloneApplication.class, args);
+    }
+
+}
