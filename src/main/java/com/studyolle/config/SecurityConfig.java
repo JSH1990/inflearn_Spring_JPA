21ebed3157511896a4f49e4a1fce92b16e5b702f
jshblack <118979741+JSH1990@users.noreply.github.com>
2025-10-11 23:11:38 +0900

    account

---
+++ src/main/java/com/studyolle/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.studyolle.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity //시큐리티 커스텀 설정
+public class SecurityConfig {
+}
