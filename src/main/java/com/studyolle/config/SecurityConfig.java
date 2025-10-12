708addb8c0c357b892e7cdb90f4c433326c6e113
jshblack <118979741+JSH1990@users.noreply.github.com>
2025-10-12 22:25:44 +0900

    account controller test

--- src/main/java/com/studyolle/config/SecurityConfig.java
+++ src/main/java/com/studyolle/config/SecurityConfig.java
@@ -1,9 +1,38 @@
 package com.studyolle.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity //시큐리티 커스텀 설정
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/login", "/sign-up", "/check-email-token",
+                                "/email-login", "/login-by-email", "/search/study").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/profile/*").permitAll()
+                        .anyRequest().authenticated()
+//                )
+//                .formLogin(form -> form
+//                        .loginPage("login")
+//                        .defaultSuccessUrl("/", true)
+//                        .permitAll()
+//                )
+//                .logout(logout -> logout
+//                        .logoutUrl("/logout")
+//                        .logoutSuccessUrl("/login")
+//                        .permitAll()
+                );
+        return http.build();
+    }
+
 }
