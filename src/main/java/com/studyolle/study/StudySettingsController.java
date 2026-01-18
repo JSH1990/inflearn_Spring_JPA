6b2b0e89caba184f12773a0e7e5cf95dccb1df86
JSH1990 <tmdgus21@naver.com>
2026-01-18 21:31:55 +0100

    study settings

---
+++ src/main/java/com/studyolle/study/StudySettingsController.java
@@ -0,0 +1,34 @@
+package com.studyolle.study;
+
+
+import com.studyolle.account.CurrentAccount;
+import com.studyolle.domain.Account;
+import com.studyolle.domain.Study;
+import com.studyolle.study.form.StudyDescriptionForm;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/study/{path}/settings")
+@RequiredArgsConstructor
+public class StudySettingsController {
+
+    private final StudyService studyService;
+    private final ModelMapper modelMapper;
+
+    @GetMapping("/description")
+    public String viewStudySetting(@CurrentAccount Account account, @PathVariable String path, Model model) {
+        Study study = studyService.getStudyToUpdate(account, path);
+        model.addAttribute(account);
+        model.addAttribute(study);
+        model.addAttribute(modelMapper.map(study, StudyDescriptionForm.class));
+        return  "study/settings/description";
+    }
+
+
+}
