1cef807544175b0a7bbbaaa3f81c3eb6fb04cfa1
JSH1990 <tmdgus21@naver.com>
2026-02-08 21:32:24 +0100

    study select

--- src/main/java/com/studyolle/domain/Study.java
+++ src/main/java/com/studyolle/domain/Study.java
@@ -20,8 +20,15 @@ import java.util.Set;
         @NamedAttributeNode("tags"),
         @NamedAttributeNode("zones"),
         @NamedAttributeNode("managers"),
-        @NamedAttributeNode("members")
-})
+        @NamedAttributeNode("members")})
+@NamedEntityGraph(name = "Study.withTagsAndManagers", attributeNodes = {
+        @NamedAttributeNode("tags"),
+        @NamedAttributeNode("managers")})
+@NamedEntityGraph(name = "Study.withZonesAndManagers", attributeNodes = {
+        @NamedAttributeNode("zones"),
+        @NamedAttributeNode("managers")})
+@NamedEntityGraph(name = "Study.withManagers", attributeNodes = {
+        @NamedAttributeNode("managers")})
 @Entity
 @Getter @Setter @EqualsAndHashCode(of = "id")
 @Builder @AllArgsConstructor @NoArgsConstructor
@@ -31,7 +38,7 @@ public class Study {
     private Long id;
 
     @ManyToMany
-    private Set<Account> managers = new HashSet<>();//관리자는 중복되면 안 되므로 Set
+    private Set<Account> managers = new HashSet<>();
 
     @ManyToMany
     private Set<Account> members = new HashSet<>();
@@ -44,7 +51,7 @@ public class Study {
     private String shortDescription;
 
     @Lob @Basic(fetch = FetchType.EAGER)
-    private String fullDescription; //전체 본문
+    private String fullDescription;
 
     @Lob @Basic(fetch = FetchType.EAGER)
     private String image;
@@ -55,21 +62,21 @@ public class Study {
     @ManyToMany
     private Set<Zone> zones = new HashSet<>();
 
-    private LocalDateTime publishedDateTime; //스터디 공개시간
+    private LocalDateTime publishedDateTime;
 
-    private LocalDateTime closedDateTime; //스터디 종료시간
+    private LocalDateTime closedDateTime;
 
-    private LocalDateTime recruitingUpdatedDateTime; //인원모집시간 제한
+    private LocalDateTime recruitingUpdatedDateTime;
 
-    private boolean recruiting; //인원모집중인지 여부
+    private boolean recruiting;
 
-    private boolean published; //공개 여부
+    private boolean published;
 
-    private boolean closed; //종료 여부
+    private boolean closed;
 
-    private boolean useBanner; //배너 사용 유무
+    private boolean useBanner;
 
-    public void addManager(Account account){
+    public void addManager(Account account) {
         this.managers.add(account);
     }
 
@@ -86,5 +93,53 @@ public class Study {
 
     public boolean isManager(UserAccount userAccount) {
         return this.managers.contains(userAccount.getAccount());
+    }
+
+    public void addMemeber(Account account) {
+        this.members.add(account);
+    }
+
+    public String getImage() {
+        return image != null ? image : "/images/default_banner.png";
+    }
+
+    public void publish() {
+        if (!this.closed && !this.published) {
+            this.published = true;
+            this.publishedDateTime = LocalDateTime.now();
+        } else {
+            throw new RuntimeException("스터디를 공개할 수 없는 상태입니다. 스터디를 이미 공개했거나 종료했습니다.");
+        }
+    }
+
+    public void close() {
+        if (this.published && !this.closed) {
+            this.closed = true;
+            this.closedDateTime = LocalDateTime.now();
+        } else {
+            throw new RuntimeException("스터디를 종료할 수 없습니다. 스터디를 공개하지 않았거나 이미 종료한 스터디입니다.");
+        }
+    }
+
+    public void startRecruit() {
+        if (canUpdateRecruiting()) {
+            this.recruiting = true;
+            this.recruitingUpdatedDateTime = LocalDateTime.now();
+        } else {
+            throw new RuntimeException("인원 모집을 시작할 수 없습니다. 스터디를 공개하거나 한 시간 뒤 다시 시도하세요.");
+        }
+    }
+
+    public void stopRecruit() {
+        if (canUpdateRecruiting()) {
+            this.recruiting = false;
+            this.recruitingUpdatedDateTime = LocalDateTime.now();
+        } else {
+            throw new RuntimeException("인원 모집을 멈출 수 없습니다. 스터디를 공개하거나 한 시간 뒤 다시 시도하세요.");
+        }
+    }
+
+    public boolean canUpdateRecruiting() {
+        return this.published && this.recruitingUpdatedDateTime == null || this.recruitingUpdatedDateTime.isBefore(LocalDateTime.now().minusHours(1));
     }
 }
